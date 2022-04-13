39d4c69544601ebde1e4f94e68fdc0b83f45ec54
Mateus Riva <mateus.riva@telecom-paristech.fr>
2022-04-13 13:36:08 +0200

    Fix graph relations for other configs.

--- run_experiment_strict.py
+++ run_experiment_strict.py
@@ -109,7 +109,7 @@ if __name__ == "__main__":
     #dataset_size = 400
     for dataset_size in [10000]:
         # Preparing the foreground
-        fg_label = "D"  # Change here for other configurations. It's ugly, I know.
+        fg_label = "H"  # Change here for other configurations. It's ugly, I know.
 
         if fg_label == "T":  # Right triangle
             fg_classes = [0, 1, 8]
@@ -117,18 +117,28 @@ if __name__ == "__main__":
             position_translation=0.5
             position_noise=0
             bg_bboxes = (0.4, 0.0, 0.9, 0.5)
+            graph_relations = [[1, 2, 0, -0.4],
+                               [1, 3, 0.3, -0.4],
+                               [2, 3, 0.3, 0]]   
         elif fg_label == "D":
             fg_classes = [0, 1, 8, 9]
             base_fg_positions = [(0.5, 0.3), (0.7, 0.5), (0.5, 0.7), (0.3, 0.5)]
             position_translation=0.5
             position_noise=0
             bg_bboxes = (0.25, 0.0, 0.75, 0.55)
+            graph_relations = [[1, 2, -0.2, -0.2],
+                               [1, 3, 0.0, -0.4],
+                               [1, 4, 0.2, -0.2],
+                               [2, 3, 0.2, -0.2],
+                               [2, 4, 0.4, 0.0],
+                               [3, 4, 0.2, 0.2]] 
         elif fg_label == "H":
             fg_classes = [0, 1]
             base_fg_positions = [(0.5, 0.3), (0.5, 0.7)]
             position_translation=0.5
             position_noise=0
             bg_bboxes = (0.25, 0.0, 0.75, 0.55)
+            graph_relations = [[1, 2, 0, -0.4]]
         else: raise ValueError("fg_label {} not recognized".format(fg_label))
 
         # Also setting the image dimensions in advance
@@ -138,9 +148,6 @@ if __name__ == "__main__":
         crit_classes = [0,1]  # BG and first class (shirts)
 
         # Preparing the relations
-        graph_relations = [[1, 2, 0, -0.4],
-                        [1, 3, 0.3, -0.4],
-                        [2, 3, 0.3, 0]]   
         relational_criterion = SpatialPriorErrorSegmentation(graph_relations, image_dimensions=image_dimensions,
                                                              num_classes=len(fg_classes), crit_classes=crit_classes)
 
